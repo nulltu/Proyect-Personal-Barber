e52f08e1ed903b4f28ebb58927ba4332232f6f38
nulltu <rusbent.matta@gmail.com>
2020-10-12 17:05:10 -0300

    root

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import rootReducer from './redux/rootReducer'
+
+const myStore = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
+  <Provider store={myStore}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+document.getElementById('root')
+)
 
 serviceWorker.unregister();
